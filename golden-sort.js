843ad3ece2900328d53e580d6fcd992efd46903a
Javier Cejudo <javier@javiercejudo.com>
2015-03-01 23:04:20 +1100

    feat(sort): fix sorting and add travis config

--- golden-sort.js
+++ golden-sort.js
@@ -8,32 +8,19 @@ function swap(arraySwap, positionA, positionB) {
   return arraySwap;
 }
 
-function getOptimalStrategyStrict(n) {
-  var strategyCandidate = Math.floor(n / Math.E),
-    sum = 0,
-    i;
-
-  for(i = strategyCandidate + 2; i <= n; i += 1) {
-    sum += 1 / (i - 1);
-  }
-
-  return (sum > 1) ? strategyCandidate + 1 : strategyCandidate;
-}
-
 function getOptimalStrategyFast(n) {
   return Math.round(n / Math.E);
 }
 
-function goldenSort(_arrayToSort) {
-  if (_arrayToSort.length < 2) {
-    return _arrayToSort;
+
+function goldenSort(arrayToSort) {
+  if (arrayToSort.length < 2) {
+    return arrayToSort;
   }
 
-  var arrayToSort = _arrayToSort.slice(0),
-    currentStrategy,
-    relevantArrayLenght = arrayToSort.length,
-    relevantArrayLastIndex = relevantArrayLenght - 1,
-    swaps,
+  var currentStrategy,
+    relevantArrayLength = arrayToSort.length,
+    relevantArrayLastIndex = relevantArrayLength - 1,
     currentMax,
     currentMaxIndex,
     index,
@@ -42,8 +29,7 @@ function goldenSort(_arrayToSort) {
   do {
     currentMax = -Infinity;
     index = 0;
-    swaps = 1;
-    currentStrategy = getOptimalStrategyFast(relevantArrayLenght);
+    currentStrategy = getOptimalStrategyFast(relevantArrayLength);
 
     do {
       if (currentMax < arrayToSort[index]) {
@@ -54,41 +40,27 @@ function goldenSort(_arrayToSort) {
       index += 1;
     } while (index < currentStrategy);
 
-    if (index !== relevantArrayLastIndex && arrayToSort[relevantArrayLastIndex] < arrayToSort[currentMaxIndex]) {
+    if (currentMaxIndex !== relevantArrayLastIndex && arrayToSort[relevantArrayLastIndex] < arrayToSort[currentMaxIndex]) {
        swap(arrayToSort, relevantArrayLastIndex, currentMaxIndex);
-
-       if (arrayToSort[relevantArrayLastIndex - 1] < arrayToSort[currentMaxIndex]) {
-         swap(arrayToSort, relevantArrayLastIndex - 1, currentMaxIndex);
-       }
     }
 
     do {
       if (currentMax < arrayToSort[index]) {
         if (index !== relevantArrayLastIndex && arrayToSort[relevantArrayLastIndex] < arrayToSort[index]) {
           swap(arrayToSort, relevantArrayLastIndex, index);
-
-          if (arrayToSort[relevantArrayLastIndex - 1] < arrayToSort[index]) {
-            swap(arrayToSort, relevantArrayLastIndex - 1, index);
-          }
         }
       }
 
       index += 1;
-    } while (index < relevantArrayLenght);
+    } while (index < relevantArrayLength);
 
-    relevantArrayLenght -= swaps;
-    relevantArrayLastIndex -= swaps;
-  } while (relevantArrayLenght > 1);
+    relevantArrayLength -= 1;
+    relevantArrayLastIndex -= 1;
+  } while (relevantArrayLength > 1);
 
   return arrayToSort;
 }
 
-exports.getOptimalStrategyStrict = getOptimalStrategyStrict;
 exports.getOptimalStrategyFast = getOptimalStrategyFast;
 exports.swap = swap;
 exports.sort = goldenSort;
-
-// var result = goldenSort([6, 9, 5, 1, 4, 3, 0, 2, 8, 7]);
-var result = goldenSort([6, 7, 8, 9, 1, 2, 3, 4, 5]);
-//
-// console.log(result);
--- test.js
+++ test.js
@@ -3,36 +3,6 @@ var goldenSort = require('./golden-sort.js');
 var shuffle = require('knuth-shuffle').knuthShuffle
 
 describe('Golden Sort Suite', function() {
-  describe('getOptimalStrategyStrict', function() {
-    it('should return the expected strategies', function() {
-      var optimalStrategies = [
-        { size:       1, strategy:       0 },
-        { size:       2, strategy:       0 },
-        { size:       3, strategy:       1 },
-        { size:       4, strategy:       1 },
-        { size:       5, strategy:       2 },
-        { size:      10, strategy:       3 },
-        { size:      15, strategy:       5 },
-        { size:      20, strategy:       7 },
-        { size:     271, strategy:     100 },
-        { size:    1359, strategy:     500 },
-        { size:    2718, strategy:    1000 },
-        { size:   13591, strategy:    5000 },
-        { size:   27182, strategy:   10000 },
-        { size:  135914, strategy:   50000 },
-        { size:  271828, strategy:  100000 },
-        { size: 1359141, strategy:  500000 },
-        { size: 2718281, strategy: 1000000 },
-        { size: 5436563, strategy: 2000000 }
-      ];
-
-      optimalStrategies.forEach(function (optimalStrategy) {
-        goldenSort.getOptimalStrategyStrict(optimalStrategy.size)
-          .should.be.exactly(optimalStrategy.strategy);
-      });
-    });
-  });
-
   describe('getOptimalStrategyLoose', function() {
     it('should return the expected strategies', function() {
       var optimalStrategies = [
@@ -75,7 +45,7 @@ describe('Golden Sort Suite', function() {
       var integers = [];
 
       for (var i = 0; i < 100; i += 1) {
-        integers.push(i);
+        integers[i] = i;
       }
 
       goldenSort.sort(shuffle(integers.slice(0))).should.eql(integers);
